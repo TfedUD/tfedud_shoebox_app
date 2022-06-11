ffb61548d3a648ae7986412e020ca8be8dc24027
trevor <63176386+TfedUD@users.noreply.github.com>
2022-06-11 13:04:02 -0400

    chore: refactor

--- app/app.py
+++ app/app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.write("Welcome! Start writing your Pollination app here.")
+st.set_page_config(
+    page_title='UD Shoebox Study App',
+    layout='wide',
+    page_icon='./assets/UD_Logo.png'
+)
