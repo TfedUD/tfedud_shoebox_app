7f6937afd20ba5412d422d1b511baaeac302c48a
trevor <63176386+TfedUD@users.noreply.github.com>
2022-06-13 12:23:34 -0400

    feat: changed theme, added neater UI

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 from streamlit_vtkjs import st_vtkjs
 from pathlib import Path
 import json
@@ -15,11 +16,22 @@ from hb_utils.add_aps import add_aps_by_ratio, add_louver_shade
 
 st.set_page_config(
     page_title='UD Shoebox Study App',
-    layout='centered',
+    layout='wide',
     page_icon='./assets/UD_Logo.png'
 )
 
 st.sidebar.image('./assets/UD_Logo.png')
+st.sidebar.write(""" Going to add the log in/
+                 API key thing, as well as
+                 'how many runs' the selected
+                 will produce""")
+
+with st.expander("Climate Set"):
+    show_map = st.button('Show EPW Map')
+    epw_url = st.text_input(label='enter epw URL')
+    if show_map:
+        components.iframe(src="https://www.ladybug.tools/epwmap/",
+                          width=900, height=650, scrolling=True)
 
 
 def add_viewer(model_vtk):
@@ -28,74 +40,79 @@ def add_viewer(model_vtk):
     )
 
 
-st.sidebar.subheader("Room Data")
-wid = st.sidebar.number_input('Width', value=4.5)
-dep = st.sidebar.number_input('Depth', value=6.5)
-hei = st.sidebar.number_input('Height', value=3.5)
+with st.expander("Room, Aperture & Shade Geometry Inputs"):
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.subheader("Room Data")
+        wid = st.number_input('Width', value=4.5)
+        dep = st.number_input('Depth', value=6.5)
+        hei = st.number_input('Height', value=3.5)
 
-st.sidebar.subheader("Aperature Data")
-wwr = st.sidebar.multiselect('WWR', [0.20,
-                                     0.30,
-                                     0.40,
-                                     0.50,
-                                     0.60,
-                                     0.70,
-                                     0.80], default=0.50)
+    with col2:
+        st.subheader("Aperature Data")
+        wwr = st.multiselect(
+            'WWR', [0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80],
+            default=0.50)
 
-sub_d = st.sidebar.checkbox('Subdivision', value=True)
-sub_d = True if sub_d == True else False
+        sub_d = st.checkbox('Subdivision', value=True)
+        sub_d = True if sub_d == True else False
 
-win_height = st.sidebar.multiselect('Window Height', [0.45,
-                                                      0.5,
-                                                      1.0,
-                                                      1.5,
-                                                      2.0,
-                                                      2.5,
-                                                      3.0,
-                                                      3.5], default=1.0)
+        win_height = st.multiselect(
+            'Window Height', [0.45, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5],
+            default=1.0)
 
-sill_height = st.sidebar.multiselect('Sill Height', [0.2,
-                                                     0.3,
-                                                     0.4,
-                                                     0.5,
-                                                     0.6,
-                                                     0.7,
-                                                     0.8,
-                                                     0.9,
-                                                     1.0], default=0.2)
+        sill_height = st.multiselect(
+            'Sill Height', [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+            default=0.2)
 
-horiz_sep = st.sidebar.multiselect('Horiz Separation', [1.0,
-                                                        2.0,
-                                                        3.0], default=1.0)
+        horiz_sep = st.multiselect('Horiz Separation', [1.0, 2.0, 3.0], default=1.0)
 
-vert_sep = st.sidebar.multiselect('Vertical Separation', [0.0,
-                                                          1.0,
-                                                          2.0,
-                                                          3.0], default=0.0)
+        vert_sep = st.multiselect(
+            'Vertical Separation', [0.0, 1.0, 2.0, 3.0],
+            default=0.0)
 
-st.sidebar.subheader("Shade Data")
-louv_d = st.sidebar.multiselect('Depth', [0.00,
-                                          0.10,
-                                          0.20,
-                                          0.30,
-                                          0.40], default=0.00)
+    with col3:
 
+        st.subheader("Shade Data")
+        louv_d = st.multiselect('Depth', [0.00, 0.10, 0.20, 0.30, 0.40], default=0.00)
 
-louv_spc = st.sidebar.multiselect("Shade Spacing", [0.00,
-                                                    0.15,
-                                                    0.25,
-                                                    0.35,
-                                                    0.45,
-                                                    0.55], default=0.00)
+        louv_spc = st.multiselect(
+            "Shade Spacing", [0.00, 0.15, 0.25, 0.35, 0.45, 0.55],
+            default=0.00)
 
-louv_offset = st.sidebar.multiselect("Facade offset", [0.00,
-                                                       0.10,
-                                                       0.20,
-                                                       0.30, ], default=0.00)
+        louv_offset = st.multiselect(
+            "Facade offset", [0.00, 0.10, 0.20, 0.30, ],
+            default=0.00)
 
+        louv_poz = st.checkbox("Vertical Shades", value=None)
+        louv_poz = True if louv_poz == True else False
 
-louv_poz = st.sidebar.checkbox("Vertical Shades", value=None)
-louv_poz = True if louv_poz == True else False
+with st.expander("Baseline Typology Data"):
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write("""#### Construction Set""")
+        _climate = st.selectbox('Climate Zone',
+                                ('1-Very Hot', '2-Hot', '3-Warm',
+                                    '4-Mixed', '5-Cool', '6-Cold',
+                                    '7-Very Cold', '8-Subartic')
+                                )
+        _vintage = st.selectbox('Building Vintage',
+                                ('ASHRAE 90.1 2019', 'ASHRAE 90.1 2016',
+                                    'AHSRAE 90.1 2013')
+                                )
+        _constr_type = st.selectbox('Baseline Construction Type',
+                                    ('SteelFramed', 'WoodFramed',
+                                        'Metal Building', 'Mass')
+                                    )
+
+    with col2:
+        st.write("""#### Zone Program""")
+        _bldg_typol = st.selectbox(
+            'Building Use',
+            ('LargeOffice', 'MediumOffice', 'MidriseApartment', 'HighriseApartment',))
+        st.write("""Need to add a widget for programs""")
+
 
 _send_it = st.sidebar.button('Accept Inputs')
 
