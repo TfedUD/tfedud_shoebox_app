a751849008700c68da7bbb4600fae1c6482f58cb
trevor <63176386+TfedUD@users.noreply.github.com>
2022-06-13 11:47:06 -0400

    feat: multiselect input

--- app/app.py
+++ app/app.py
@@ -32,19 +32,68 @@ st.sidebar.subheader("Room Data")
 wid = st.sidebar.number_input('Width', value=4.5)
 dep = st.sidebar.number_input('Depth', value=6.5)
 hei = st.sidebar.number_input('Height', value=3.5)
+
 st.sidebar.subheader("Aperature Data")
-wwr = st.sidebar.number_input('WWR', value=0.55)
+wwr = st.sidebar.multiselect('WWR', [0.20,
+                                     0.30,
+                                     0.40,
+                                     0.50,
+                                     0.60,
+                                     0.70,
+                                     0.80], default=0.50)
+
 sub_d = st.sidebar.checkbox('Subdivision', value=True)
 sub_d = True if sub_d == True else False
-win_height = st.sidebar.number_input('Window Height', value=2.00)
-sill_height = st.sidebar.number_input('Sill Height', value=0.8)
-horiz_sep = st.sidebar.number_input('Horiz Separation', value=3.0)
-vert_sep = st.sidebar.number_input('Vertical Separation', value=0.0)
+
+win_height = st.sidebar.multiselect('Window Height', [0.45,
+                                                      0.5,
+                                                      1.0,
+                                                      1.5,
+                                                      2.0,
+                                                      2.5,
+                                                      3.0,
+                                                      3.5], default=1.0)
+
+sill_height = st.sidebar.multiselect('Sill Height', [0.2,
+                                                     0.3,
+                                                     0.4,
+                                                     0.5,
+                                                     0.6,
+                                                     0.7,
+                                                     0.8,
+                                                     0.9,
+                                                     1.0], default=0.2)
+
+horiz_sep = st.sidebar.multiselect('Horiz Separation', [1.0,
+                                                        2.0,
+                                                        3.0], default=1.0)
+
+vert_sep = st.sidebar.multiselect('Vertical Separation', [0.0,
+                                                          1.0,
+                                                          2.0,
+                                                          3.0], default=0.0)
 
 st.sidebar.subheader("Shade Data")
-louv_d = st.sidebar.number_input("Depth", value=0.15)
-louv_spc = st.sidebar.number_input("Shade Spacing", value=0.50)
-louv_offset = st.sidebar.number_input("Facade offset", value=0.0)
+louv_d = st.sidebar.multiselect('Depth', [0.00,
+                                          0.10,
+                                          0.20,
+                                          0.30,
+                                          0.40], default=0.00)
+
+
+louv_spc = st.sidebar.multiselect("Shade Spacing", [0.00,
+                                                    0.15,
+                                                    0.25,
+                                                    0.35,
+                                                    0.45,
+                                                    0.55], default=0.00)
+
+louv_offset = st.sidebar.multiselect("Facade offset", [0.00,
+                                                       0.10,
+                                                       0.20,
+                                                       0.30, ], default=0.00)
+
+
 louv_poz = st.sidebar.checkbox("Vertical Shades", value=None)
 louv_poz = True if louv_poz == True else False
 
@@ -56,18 +105,18 @@ if _send_it:
     wall_faces = [face.duplicate() for face in rm.faces if str(face.type) == 'Wall']
     front_face_ap = add_aps_by_ratio(
         wall_faces[2],
-        _ratio=[wwr],
+        _ratio=[wwr[0]],
         _subdivide_=[sub_d],
-        _win_height_=[win_height],
-        _sill_height_=[sill_height],
-        _horiz_separ_=[horiz_sep],
-        vert_separ_=[vert_sep])
+        _win_height_=[win_height[0]],
+        _sill_height_=[sill_height[0]],
+        _horiz_separ_=[horiz_sep[0]],
+        vert_separ_=[vert_sep[0]])
 
     front_face_ap = add_louver_shade(
         [front_face_ap],
-        _depth=[louv_d],
-        _dist_between_=[louv_spc],
-        _facade_offset_=[louv_offset],
+        _depth=[louv_d[0]],
+        _dist_between_=[louv_spc[0]],
+        _facade_offset_=[louv_offset[0]],
         vertical_=[louv_poz])
 
     left_face = wall_faces[1]
